5b0a8b81b3f0a48e959d2b496f26e72d7b70a785
Athira612 <74651653+Athira612@users.noreply.github.com>
2021-03-10 14:55:38 +0530

    Add files via upload

---
+++ prgm/disjointset.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+struct DisjSet {
+int parent[10];
+int rank[10];
+int n;
+}dis;
+
+void makeSet()
+{
+for (int i = 0;i < dis.n; i++)
+    {
+dis.parent[i] = i;
+dis.rank[i]=0;
+}
+}
+
+void displaySet()
+{
+printf("\nParent Array\n");
+for (int i = 0; i <dis.n; i++) {
+printf("%d",dis.parent[i]); }
+printf("\nRank Array\n");
+for (int i = 0; i &lt; dis.n; i++)
+{
+printf("%d",dis.rank[i]);
+}
+printf("\n");
+}
+
+int find(int x)
+{
+if(dis.parent[x])
+{
+dis.parent[x]=find(dis.parent[x]);
+}
+return dis.parent[x];
+}
+
+void union(int x,int y)
+{
+int xset=find(x);
+int yset=find(y);
+if xset==yset)
+return;
+}
+if(dis.rank[xset]< dis.rank [yset])
+{
+dis.parent[xset]=yset;
+dis.rank[xset]=-1;
+}
+else if (dis.rank[xset]>dis.rank[yset])
+{
+dis.parent[yset]=xset;
+dis.rank[yset]=-1;
+}
+else
+{
+dis.parent[yset]=xset;
+dis,rank[xset]=dis.rank[xset]+1;
+dis.rank[yset]=-1;
+}
+}
+int main()
+{ int n,x,y;
+printf("How many elements ?");
+scanf("%d",dis.n);
+makeSet();
+int ch,wish;
+
+do
+{
+print("\n ____Menu___\n");
+printf("1. UNION \n 2.find\n 3.display\n");
+print("enter choice\n");
+scanf("%d",&ch);
+switch(ch);
+{
+    case 1: printf("Enter elements to perform union");
+scanf("%d %d"&x,&y);
+Union(x, y);
+break;
+case 2: printf("Enter elements to check if connected components");
+scanf("%d %d" ,&x,&y);
+if (find(x) == find(y))
+printf("Connected components\n") ;
+else
+printf("Not onnected components \n&quot;) ;
+break;
+case 3: displaySet();
+break;
+}
+printf("\nDo you wish to continue ?(1/0)\n");
+scanf(&"%d",&wish);
+}while(wish==1);
+return 0;
+}
